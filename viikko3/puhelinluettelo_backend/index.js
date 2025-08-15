3d1ad42cadb7d65a4737184df3e41df6dc270901
Anna Salminiitty <koivu05@gmail.com>
2025-08-15 10:18:38 +0300

    exercises 3.12-14

--- viikko3/puhelinluettelo_backend/index.js
+++ viikko3/puhelinluettelo_backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -10,26 +12,27 @@ morgan.token('object', function(req, res) {
     return JSON.stringify(req.body)
 })
 
-let persons = [
-    { id: "1", name: 'Arto Hellas', number: '040-123456' },
-    { id: "2", name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: "3", name: 'Dan Abramov', number: '12-43-234345' },
-    { id: "4", name: 'Mary Poppendieck', number: '39-23-6423122---' }
-  ]
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(people => {
+        res.json(people)
+    })
 })
 
 app.post('/api/persons', (req, res) => {
     if (!req.body.name || !req.body.number) {
-        return res.status(400).json({
-            error: 'name or number missing'
-        })
+        return res.status(400).json({error:'name or number missing'})
     }
-    const person = { ...req.body}
+    const person = new Person({
+        name: req.body.name,
+        number: req.body.number
+    })
+    
+    person.save().then(savedPerson => {
+        res.status(201).json(savedPerson)
+    })
+})
 
-    if(persons.find(object => object.name === person.name)) {
+    /*if(persons.find(object => object.name === person.name)) {
         return res.status(400).json({
             error: 'name must be unique'
         })
@@ -37,7 +40,7 @@ app.post('/api/persons', (req, res) => {
     person.id = String(Math.floor(Math.random()*10000))
     persons = persons.concat(person)
     res.json(person)
-  })
+  })*/
 
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
---
+++ viikko3/puhelinluettelo_backend/models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB', error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnObject) => {
+        returnObject.id = returnObject._id.toString()
+        delete returnObject.id
+        delete returnObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
