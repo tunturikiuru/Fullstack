3b384166bc1f53920e3aabd940a7443ed85f28ef
Anna Salminiitty <koivu05@gmail.com>
2025-08-15 22:01:25 +0300

    exercise 2.20

--- viikko2/maiden_tiedot/src/App.jsx
+++ viikko2/maiden_tiedot/src/App.jsx
@@ -11,9 +11,9 @@ const Button = ({country, setChosenCountry}) => {
   return <button key={country.name.common} onClick={() => setChosenCountry(country)}>Show</button>
 }
 
-const DisplayCountries = ({matchedCountries, setChosenCountry, chosenCountry}) => {
-  if (chosenCountry) {
-    return DisplayCountry(chosenCountry)
+const DisplayCountries = ({matchedCountries, setChosenCountry, chosenCountry, capital}) => {
+  if (chosenCountry && capital) {
+    return DisplayCountry(chosenCountry, capital)
   }
 
   if (matchedCountries.length > 10) {
@@ -33,7 +33,8 @@ const DisplayCountries = ({matchedCountries, setChosenCountry, chosenCountry}) =
   )
 }
 
-const DisplayCountry = (chosenCountry) => {
+const DisplayCountry = (chosenCountry, capital) => {
+  console.log(capital.weather[0].icon)
   return (
     <div>
       <h1>{chosenCountry.name.common}</h1>
@@ -43,15 +44,22 @@ const DisplayCountry = (chosenCountry) => {
         {Object.values(chosenCountry.languages).map(language => <li key={language}>{language}</li>)}
       </ul>
       <img src={chosenCountry.flags.png} alt={chosenCountry.flags.alt}/>
+      <h2>Weather in {chosenCountry.capital}</h2>
+      <p>Temperature {capital.main.temp} Celsius</p>
+      <img src={`https://openweathermap.org/img/wn/${capital.weather[0].icon}@2x.png`} alt='weather icon'/>
+      <p>Wind {capital.wind.speed} m/s</p>
     </div>
   )
 }
 
 
 function App() {
+  const api_key = import.meta.env.VITE_API_KEY
+
   const [chosenCountry, setChosenCountry] = useState(null)
   const [countries, setCountries] = useState([])
   const [inputCountry, setInputCountry] = useState('')
+  const [capital, setCapital] = useState(null)
 
   useEffect (() => {
     axios
@@ -61,9 +69,21 @@ function App() {
       })
   }, [])
 
+  useEffect (() => {
+    if (chosenCountry) {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${chosenCountry.capital}&units=metric&appid=${api_key}`)
+        .then(response => {
+          console.log(response.data)
+          setCapital(response.data)
+        })
+    }
+  }, [chosenCountry])
+
   const changeInputCountry = (event) => {
     setInputCountry(event.target.value)
     setChosenCountry(null)
+    setCapital(null)
   }
 
   const matchedCountries = () => {
@@ -81,7 +101,7 @@ function App() {
   return (
     <div>
       <CountryForm changeInputCountry={changeInputCountry} inputCountry={inputCountry} />
-      <DisplayCountries matchedCountries={matchedCountries()} setChosenCountry={setChosenCountry} chosenCountry={chosenCountry}/>
+      <DisplayCountries matchedCountries={matchedCountries()} setChosenCountry={setChosenCountry} chosenCountry={chosenCountry} capital={capital}/>
     </div>
   )
 }
