64c62cb5d94cafb397875599bb6900b49f4a0516
Anna Salminiitty <koivu05@gmail.com>
2025-09-01 20:23:27 +0300

    5.6-5.7

--- viikko5/bloglist-frontend/src/components/Blog.jsx
+++ viikko5/bloglist-frontend/src/components/Blog.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import BlogForm from "./NewBlogForm"
 
 const Blogs = ({blogs, setBlogs, logout, setMessage}) => {
@@ -16,10 +17,42 @@ const Blogs = ({blogs, setBlogs, logout, setMessage}) => {
 )}
 
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+  const hideWhenVisible = {display: blogVisible ? 'none' : ''}
+  const showWhenVisible = {display: blogVisible ? '': 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  blog.added = blog.user[0] ? blog.user[0].name : 'added by unknown user'
+
+  return(
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={() => setBlogVisible(true)}>view</button>
+      </div> 
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title} {blog.author} <button onClick={() => setBlogVisible(false)}>hide</button>
+        </div>
+        <div>
+          {blog.url} 
+        </div>
+        <div>
+          likes: {blog.likes} <button>like - not yet working</button>
+        </div>
+        <div>
+          {blog.added} 
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blogs
